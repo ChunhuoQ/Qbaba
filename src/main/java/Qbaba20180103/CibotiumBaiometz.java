7ac673b3e3c872e28c120ba3db182d3f8f4e2c69
Qbaba <Qbaba@PC-20171128XFIZ>
2018-01-04 09:31:17 +0800

    add log

--- src/main/java/Qbaba20180103/CibotiumBaiometz.java
+++ src/main/java/Qbaba20180103/CibotiumBaiometz.java
@@ -7,25 +7,25 @@
  */
 
 package Qbaba20180103;
+
 /**
- * Description:   <br/>
- * Date:     2018年1月3日 下午2:16:43 <br/>
- * @author   Qbaba
+ * Description: <br/>
+ * Date: 2018年1月3日 下午2:16:43 <br/>
+ * 
+ * @author Qbaba
  * @version
- * @see 第一题：1：笔记本USB接口；  设定一个固定接口，以后厂家的的U盘或数据线接口就得按这个接口大小做！
- *              出厂的U盘和数据线就是笔记本USB接口的实现.
- *           2：  小型蹦蹦床；仅供十岁以下孩童玩的小型游乐设施；
- *           如果超过十岁，则无法承受下进入的人的重量，会导致设备崩坏；
- *           小型蹦蹦床就是可以让十岁以下小孩来实现的接口；
- *           3：同种
+ * @see 第一题：1：笔记本USB接口； 设定一个固定接口，以后厂家的的U盘或数据线接口就得按这个接口大小做！
+ *      出厂的U盘和数据线就是笔记本USB接口的实现. 2： 小型蹦蹦床；仅供十岁以下孩童玩的小型游乐设施；
+ *      如果超过十岁，则无法承受下进入的人的重量，会导致设备崩坏； 小型蹦蹦床就是可以让十岁以下小孩来实现的接口； 3：同种
  */
-public interface CibotiumBaiometz {//创建一个公金毛狗接口;
-    
-    static final String nameself="馒头";
-    
-    static final int ageself=4;
+public interface CibotiumBaiometz {// 创建一个公金毛狗接口;
+
+    static final String nameself = "馒头";
+
+    static final int ageself = 4;
+
     /**
-     *  
+     * 
      * Description: <br/>
      * 
      * @author Qbaba
@@ -33,7 +33,7 @@ public interface CibotiumBaiometz {//创建一个公金毛狗接口;
      */
 
     public String multiply();
-    
+
     /**
      * 
      * Description: <br/>
@@ -42,6 +42,5 @@ public interface CibotiumBaiometz {//创建一个公金毛狗接口;
      * @return 繁衍后代的数量;
      */
     public int unbornQuantity();
-    
-}
 
+}
--- src/main/java/Qbaba20180103/TestNullPointerException.java
+++ src/main/java/Qbaba20180103/TestNullPointerException.java
@@ -7,28 +7,27 @@
  */
 
 package Qbaba20180103;
+
 /**
- * Description:   <br/>
- * Date:     2018年1月3日 下午4:46:43 <br/>
- * @author   Qbaba
+ * Description: <br/>
+ * Date: 2018年1月3日 下午4:46:43 <br/>
+ * 
+ * @author Qbaba
  * @version
  * @see
  */
 public class TestNullPointerException {
 
-    public static void main(String[] args) throws NullPointerException{
+    public static void main(String[] args) throws NullPointerException {
 
-        try{
-            String  name=null;
-            String x="小王头";
+        try {
+            String name = null;
+            String x = "小王头";
             name.equals(x);
-         }
-         catch(NullPointerException ee)
-         {  
+        } catch (NullPointerException ee) {
             throw new NullPointerException("这串代码空指针异常！");
-            
-             }
+
+        }
     }
 
 }
-
