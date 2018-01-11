b8b572f3abd4588e4601bb9eaed0190a7df11e4f
Qbaba <Qbaba@192.168.191.2>
2018-01-11 16:38:56 +0800

    add

---
+++ src/main/java/m01/d03/hw20180103/copy/Work3.java
@@ -0,0 +1,40 @@
+/**
+ * Project Name:dt59homework
+ * File Name:Work3.java
+ * Package Name:hw20180103
+ * Date:2018年1月3日下午2:47:46
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m01.d03.hw20180103.copy;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Description: <br/>
+ * Date: 2018年1月3日 下午2:47:46 <br/>
+ * 
+ * @author DuSen
+ * @version
+ * @see
+ */
+public class Work3 {
+    private final static Logger LOG = Logger.getLogger(Work3.class);
+
+    @SuppressWarnings("null")
+    public static void main(String[] args) {
+
+        String str = null;//定义一个字符串为null；
+        LOG.info(str);//输出
+        try {
+            str.length();//定义一个长度
+        } catch (Exception e) {//抓取 异常信息
+
+            // System.out.println(e);
+            LOG.info("异常信息");
+            e.printStackTrace();
+            LOG.info(e);
+        }
+
+    }
+}
