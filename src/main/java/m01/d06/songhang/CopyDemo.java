616ddf8193a3e613ffcb1ea3dc54661370ded01f
songhang <438442459@qq.com>
2018-01-16 21:30:38 +0800

    更改

--- src/main/java/m01/d06/songhang/CopyDemo.java
+++ src/main/java/m01/d06/songhang/CopyDemo.java
@@ -26,6 +26,11 @@ public class CopyDemo {
     private final static Logger LOG = Logger.getLogger(CopyDemo.class);
 
     public static void main(String[] args) throws IOException {
+        write();
+    }
+
+    private static void write() throws IOException {
+
         String path = "D:\\git\\songhang724\\new.txt";// 声明一个路径的属性并赋值
         String path1 = "D:\\git\\songhang724\\new1.txt";// 声明一个路径的属性并赋值
         FileInputStream fos = null;
@@ -35,16 +40,22 @@ public class CopyDemo {
             fis = new FileOutputStream(path1);
             byte[] b = new byte[24];// 定义一个byte数组
             int len = fos.read(b);// 通过fis工具从file中，读取byte数据到b数组中
-            String str = new String(b, 0, len);// b,字节数组；offset：开始读的位置 len：24
-                                               // 从开始位置后数24个字节
-            LOG.info(str);
-            if (fos.read(b) == -1) {
-                fis.write(b);
+
+            while ((len = fos.read(b)) != -1) {
+                String str = new String(b, 0, len);// b,字节数组；offset：开始读的位置
+                // len：24
+                // 从开始位置后数24个字节
+                LOG.info(str);
+                String str1 = str;
+                byte arr[] = str1.getBytes();
+                fis.write(arr);
             }
+
         } finally {
             fos.close();
             fis.close();
         }
         LOG.info("复制完成");
     }
+
 }
