29063bc280a00ee4b66f4ceb836aa01df40c43e5
l <l@DESKTOP-HE6H225.lan>
2018-01-16 18:21:50 +0800

    addlvruiling

---
+++ src/main/java/m01/d16/lvruiling/A.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name:Demo
+ * File Name:A.java
+ * Package Name:Demo20180116
+ * Date:2018年1月16日下午4:33:45
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m01.d16.lvruiling;
+
+/**
+ * Description: <br/>
+ * Date: 2018年1月16日 下午4:33:45 <br/>
+ * 
+ * @author l
+ * @version
+ * @see
+ */
+public class A {
+    int v = 100;
+}
