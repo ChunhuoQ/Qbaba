94d356c22a0f576a005e03d6bbb9404ec6e10172
ChunhuoQ <1126774706@qq.com>
2018-04-04 10:44:59 +0800

    add

---
+++ src/main/java/m04/d03/Qbaba/Dao/NewsDao.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:Qbaba
+ * File Name:QuestionInfoDao.java
+ * Package Name:m03.d28.Qbaba.Dao
+ * Date:2018年3月28日下午1:50:35
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m04.d03.Qbaba.Dao;
+
+import java.util.List;
+
+import m04.d03.Qbaba.entity.NewsInfo;
+import m04.d03.Qbaba.entity.UserInfoDaoImpl;
+
+/**
+ * Description: <br/>
+ * Date: 2018年3月28日 下午1:50:35 <br/>
+ * 
+ * @author Qbaba
+ * @version
+ * @see
+ */
+public interface NewsDao<T> {
+    boolean validateUser(UserInfoDaoImpl user);
+    List<NewsInfo> getAllNewsInfo();
+}
