94ca57f77890bd902dcbd96d68bca5586b076180
776429635 <dswl1314@sina.com>
2018-04-17 00:53:15 +0800

    ccd

---
+++ src/main/webapp/m04/d17/dusen/src/com/controller/ProductServlet.java
@@ -0,0 +1,87 @@
+package com.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.impl.BizDaoImpl;
+import com.entity.Page;
+import com.entity.Product;
+import com.service.impl.BizServiceImpl;
+
+/**
+ * Servlet implementation class ProductServlet
+ */
+public class ProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    //request.setCharacterEncoding("utf-8");
+	    
+	    BizDaoImpl bdi=new BizDaoImpl();
+	    BizServiceImpl bsi=new BizServiceImpl();
+	    bsi.setBizDao(bdi);
+
+	    List<Product> listpro=bsi.getAllProduct();
+	    HttpSession session=request.getSession();
+	   
+	    Page page1=new Page();
+	    if(page1.getCurrentpage()==0){
+	        page1.setCurrentpage(1);
+	    }else{
+	        page1.setCurrentpage(1);
+	    }
+	    int flag=bsi.count();
+        page1.setLastpage(flag);
+        
+        List<Product> listt=bsi.fen(page1);
+        request.setAttribute("page1", page1);
+        session.setAttribute("listt", listt);
+        
+        session.removeAttribute("page1");
+        
+        
+	    request.getRequestDispatcher("Jsp_Proscenium/M_ShowProduct.jsp").forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/m04/d17/dusen/src/com/util/PinFilter.java
@@ -0,0 +1,54 @@
+package com.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class PinFilter
+ */
+public class PinFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public PinFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	    System.out.println("=============走了没");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+	    HttpServletRequest request=(HttpServletRequest)req;
+	    HttpServletResponse response=(HttpServletResponse)res;
+	    request.setCharacterEncoding("utf-8");
+	    response.setCharacterEncoding("utf-8");
+	    chain.doFilter(req, res);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	    System.out.println("=============走了没");
+	}
+
+}
