9913757ce04a25f319fd5af944398b1dc248fc50
liupeng819 <362044553@qq.com>
2018-04-19 08:16:52 +0800

    add多

--- src/main/java/m04/d10/lvruiling/com/dt59/dao/impl/BizDaoImpl.java
+++ src/main/java/m04/d10/lvruiling/com/dt59/dao/impl/BizDaoImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import m04.d10.lvruiling.com.dt59.dao.BizDao;
+import m04.d10.lvruiling.com.dt59.entity.CommentInfo;
 import m04.d10.lvruiling.com.dt59.entity.NewsInfo;
 import m04.d10.lvruiling.com.dt59.entity.Page;
 import m04.d10.lvruiling.com.dt59.entity.UserInfo;
